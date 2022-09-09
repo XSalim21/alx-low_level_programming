08eed61a6cc1c0d68a66e3294b1dc7fce4736596
XSalim21 <salykim56@gmail.com>
2022-09-08 19:52:07 -0700

    my first C program love it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like biulding a multilingual puzzle");
+{puts("\"Programming is like biulding a multilingual puzzle,\n");
 	return (0);
 }
