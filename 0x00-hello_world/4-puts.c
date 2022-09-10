faebdedd41427e48896e57331d91f7e4e92edfba
XSalim21 <salykim56@gmail.com>
2022-09-09 17:04:41 -0700

    4- put c correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle,\n");
+{puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
