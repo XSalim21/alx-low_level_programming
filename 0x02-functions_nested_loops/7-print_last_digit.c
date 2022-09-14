42686d3f015700eef38dad681fba619aa9ef9c71
XSalim21 <salykim56@gmail.com>
2022-09-14 05:02:33 -0700

    7 print correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *
+ * print_last_digit - a function that prints the last digit of a number
  * Function that prints last digit 
  * @n: takes in an integer
  *
  * Return: result output
  */
-int print_last_didgit(int)
+int print_last_didgit(int n)
 {
 int ln = n % 10;
 
