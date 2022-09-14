dced48b0c65bd745b12e8ce300817b4264100b88
XSalim21 <salykim56@gmail.com>
2022-09-14 06:13:13 -0700

    10 - correction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
+ * add - a function that adds two integers and returns the result
  * Function that adds two integers and returns results
  * @one: first number imput
  * @two: second number imput
  * Return: one + two
  *
  */
-int add(int one,int two)
+int add(int one, int two)
 {
 return (one + two);
 }
